a3210889a9b6d1d04a853422a7967168ca48e577
shu2010 <s.pattnaik@garvan.org.au>
2019-10-19 01:52:41 +1100

    QC

--- code/QC_commands.R
+++ code/QC_commands.R
@@ -1,6 +1,6 @@
 ##test output
 ##QC
-p5_s50 <- readRDS("~/Documents/Syn_collab/Data/para_IBD_phen_test.rds1")
+p5_s50 <- readRDS("~/Documents/Syn_collab/Data/para_IBD_phen_test.rds")
 p5_s50_mat <- t(p5_s50)
 class(p5_s50_mat) <- "numeric"
 apply(p5_s50_mat[,-c(1:3)], 2, function(x)table(duplicated(x)))
